f60b7aa2f783a76e8ef64a1f2e3daea68be22a32
TUMISHE <estherolajide456@gmail.com>
2024-03-14 14:25:53 +0100

    caculator

---
+++ script.js
@@ -0,0 +1,27 @@
+let buttons = document.querySelectorAll(".btns");
+let screen = document.querySelector("#screen");
+
+buttons.forEach((button) => {
+   button.addEventListener("click",(e) =>{
+    if(e.target.innerText == "="){
+        let result = eval(screen.innerText);
+        screen.innerText = result;
+    }
+    else if(e.target.innerText == "AC"){
+        screen.innerText = 0
+    }
+    else if(e.target.innerText == "DEL"){
+        screen.innerText = screen.innerText.slice(0, -1);
+    }
+    else if(e.target.innerText == "×"){ 
+        screen.innerText += "*";
+    }
+   else{
+    if(screen.innerText == 0){
+        screen.innerText = e.target.innerText;
+    }else{
+        screen.innerText +=e.target.innerText
+    }
+   }
+   })
+})
